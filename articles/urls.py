f893a59cc3ebebfdf1f9daeed84470c2e0ccee6c
yousef saeedi <ysfsaeedi1994@gmail.com>
2020-06-25 11:20:08 +0430

    urls

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 
+
+app_name = "articles"
 urlpatterns = [
-    path('', views.articles_list),
+    path('', views.articles_list, name="list"),
+    path('<slug>', views.article_detail, name="detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from . import models
 
 def articles_list(request):
     articles = models.Article.objects.all().order_by('date')
     return render(request, 'articles/articleslist.html', {'articles' : articles})
+
+    
+def article_detail(request, slug):
+    return HttpResponse(slug)
